03e25561192fe4c984047ed6d793a36a25a13ae6
Jack Hunt <jackmileshunt@gmail.com>
2017-06-26 17:18:05 +0100

    Commit any straggling changes.

--- cuda_check.h
+++ cuda_check.h
@@ -5,14 +5,16 @@
 #include <cuda_runtime.h>
 #include <iostream>
 
+namespace Permutohedral {
 #define CUDA_CHECK(ans){cudaAssert((ans), __FILE__, __LINE__);}
-void cudaAssert(cudaError_t code, const char *file, int line) {
-	if (code != cudaSuccess) {
-		std::cerr << "CUDA ERROR: " << cudaGetErrorString(code) << " " << file << " " << line << std::endl;
-		exit(code);
+	void cudaAssert(cudaError_t code, const char *file, int line) {
+		if (code != cudaSuccess) {
+			std::cerr << "CUDA ERROR: " << cudaGetErrorString(code) << " " << file << " " << line << std::endl;
+			exit(code);
+		}
 	}
-}
 
 #define CUDA_POST_KERNEL_CHECK
 
+}
 #endif
